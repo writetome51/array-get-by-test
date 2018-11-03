97b0093e9c1f2f9952247fc84705fe7c303a113a
Steve Thompson <writetome51@gmail.com>
2018-11-02 22:36:39 -0600

    added IValueIndexPair dependency

--- lib/getFilteredResults.ts
+++ lib/getFilteredResults.ts
@@ -1,17 +1,19 @@
 import { errorIfNotArray } from 'basic-data-handling/errorIfNotArray';
 import { errorIfNotFunction } from 'basic-data-handling/errorIfNotFunction';
+import { IValueIndexPair } from 'value-index-pair-interface/IValueIndexPair';
+
 
 // Similar to Array.filter(), except it returns array of objects.  
-// Each object matches this interface:   {value: any,  index: integer}
+// Each object is an IValueIndexPair, which looks like this:   {value: any,  index: integer}
 // testFunction = function(currentValue, currentIndex?, array?) : return boolean
 
-export function getFilteredResults(testFunction, array): object[] {
+export function getFilteredResults(testFunction, array): IValueIndexPair[] {
 	errorIfNotFunction(testFunction);
 	errorIfNotArray(array);
 	let filteredResults = [];
-	array.filter((currentValue, currentIndex, array)=>{
-		if (testFunction(currentValue, currentIndex, array)){
-			filteredResults.push( {value: currentValue , index: currentIndex} );
+	array.filter((currentValue, currentIndex, array) => {
+		if (testFunction(currentValue, currentIndex, array)) {
+			filteredResults.push({value: currentValue, index: currentIndex});
 		}
 	});
 	return filteredResults;
