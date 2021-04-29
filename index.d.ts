ddf08ef0a568d6f5143ebe36096a9b019faa7c53
Steve Thompson <writetome51@gmail.com>
2021-04-29 13:58:03 -0600

    Fixed bug in typescript generic declaration. Upped version to 4.0.2

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 export declare function getByTest<T, X>(
 	test: (value: T, index?: number, array?: T[]) => boolean,
 	array: T[],
-	getValue?: (value: T, index?: number, array?: T[]) => T | X
-): T[] | X[];
+	getValue?: (value: T, index?: number, array?: T[]) => X
+): X[];
