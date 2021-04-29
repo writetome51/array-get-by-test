085100ebb768e0f1e97b597ef9b8167aa16ee47c
Steve Thompson <writetome51@gmail.com>
2021-04-29 13:15:55 -0600

    Modified generic in Typescript declaration. Instead of returning any[], it now specifies that it can return either T[] or X[]. Type 'X' is whatever getValue() returns, if not T

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
-export declare function getByTest<T>(
+export declare function getByTest<T, X>(
 	test: (value: T, index?: number, array?: T[]) => boolean,
 	array: T[],
-	getValue?: (value: T, index?: number, array?: T[]) => any
-): any[];
+	getValue?: (value: T, index?: number, array?: T[]) => T | X
+): T[] | X[];
